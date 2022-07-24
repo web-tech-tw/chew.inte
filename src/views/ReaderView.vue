9c6e4fe9375a310cead2dcb9e5e1b538d7fd7322
SuperSonic <supersonic@livemail.tw>
2022-07-24 11:03:20 +0800

    Update

--- src/views/ReaderView.vue
+++ src/views/ReaderView.vue
@@ -2,8 +2,17 @@
   <div class="container mx-auto my-5 p-5">
     <div class="mx-auto max-w-sm rounded overflow-hidden shadow-lg">
       <div class="px-6 py-4">
-        <p class="text-gray-700 text-base">
-          {{ gum.content }}
+        <div v-if="codeMode">
+          <prism-editor
+            class="code-editor"
+            v-model="content"
+            ref="codeEditor"
+            :highlight="highlighter"
+            line-numbers
+          ></prism-editor>
+        </div>
+        <p v-else class="text-gray-700 text-base">
+          {{ content }}
         </p>
       </div>
       <div v-if="gum.tags" class="px-6 pt-4 pb-2">
@@ -35,21 +44,75 @@
 </template>
 
 <script>
+// import Prism Editor
+import { PrismEditor } from "vue-prism-editor";
+import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
+
+// import highlighting library (you can use any library you want just return html string)
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism-tomorrow.css"; // import syntax highlighting styles
+
 export default {
   name: "ReaderView",
+  components: {
+    PrismEditor,
+  },
   props: {
     code: {
       type: String,
       required: true,
     },
   },
+  computed: {
+    content() {
+      return this.gum.content || "";
+    },
+  },
   data: () => ({
+    codeMode: true,
     gum: {},
   }),
+  methods: {
+    highlighter(code) {
+      return highlight(code, languages.js); // languages.<insert language> to return html with markup
+    },
+  },
   created() {
+    // Fetch data
     this.$axios.get(`/anonymous/${this.code}`).then((response) => {
       this.gum = response.data;
     });
   },
+  mounted() {
+    // Disable edit mode
+    const editorTextarea = this.$refs.codeEditor?.$el?.children[1]?.children[0];
+    if (editorTextarea) {
+      editorTextarea.setAttribute("disabled", true);
+    } else {
+      console.error("editorTextarea is not found.");
+    }
+  },
 };
 </script>
+
+<style>
+/* required class */
+.code-editor {
+  /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+  background: #2d2d2d;
+  color: #ccc;
+
+  /* you must provide font-family font-size line-height. Example: */
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px;
+}
+
+/* optional class for removing the outline */
+.prism-editor__textarea:focus {
+  outline: none;
+}
+</style>
--- src/views/WriterView.vue
+++ src/views/WriterView.vue
@@ -132,4 +132,3 @@ export default {
   outline: none;
 }
 </style>
-
