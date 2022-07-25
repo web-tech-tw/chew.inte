0db25b5bd338b7370015e064086ea39b31ff35a9
SuperSonic <supersonic@livemail.tw>
2022-07-25 13:21:25 +0800

    Update

--- src/views/ReaderView.vue
+++ src/views/ReaderView.vue
@@ -1,49 +1,144 @@
 <template>
-  <div class="container mx-auto my-5 p-5">
-    <div class="mx-auto max-w-sm rounded overflow-hidden shadow-lg">
-      <div class="px-6 py-4">
-        <div v-if="codeMode">
-          <prism-editor
-            class="code-editor"
-            v-model="content"
-            ref="codeEditor"
-            :highlight="highlighter"
-            line-numbers
-          ></prism-editor>
+  <div>
+    <div v-if="!isNotFound">
+      <div class="container mx-auto my-5 p-5">
+        <div class="mx-auto max-w-sm rounded overflow-hidden shadow-lg">
+          <div class="px-6 py-4">
+            <div v-if="isCodeMode">
+              <prism-editor
+                class="code-editor"
+                v-model="content"
+                ref="codeEditor"
+                :highlight="highlighter"
+                line-numbers
+              ></prism-editor>
+            </div>
+            <p v-else class="text-gray-700 text-base">
+              {{ content }}
+            </p>
+          </div>
+          <div class="px-6 pt-4 pb-2">
+            <span
+              v-for="(time, key) in gumTimes"
+              :key="key"
+              class="
+                inline-block
+                bg-gray-200
+                rounded-full
+                px-3
+                py-1
+                text-sm
+                font-semibold
+                text-gray-700
+                mr-2
+                mb-2
+              "
+            >
+              {{ getTimeTitle(key) }}：{{ time }}
+            </span>
+          </div>
         </div>
-        <p v-else class="text-gray-700 text-base">
-          {{ content }}
-        </p>
-      </div>
-      <div v-if="gum.tags" class="px-6 pt-4 pb-2">
-        <span
-          v-for="(tag, index) in gum.tags"
-          :key="index"
-          class="
-            inline-block
-            bg-gray-200
-            rounded-full
-            px-3
-            py-1
-            text-sm
-            font-semibold
-            text-gray-700
-            mr-2
-            mb-2
-          "
-        >
-          #{{ tag }}
-        </span>
       </div>
     </div>
-    <div>
-      {{ new Date(gum.created_at * 1000) }}<br />
-      {{ new Date(gum.updated_at * 1000) }}
+    <div v-else>
+      <div
+        class="
+          mt-10
+          mx-auto
+          py-10
+          max-w-7xl
+          px-4
+          sm:mt-12 sm:px-6
+          md:mt-16
+          lg:mt-20 lg:px-8
+          xl:mt-28
+        "
+      >
+        <div class="sm:text-center lg:text-left">
+          <h1
+            class="
+              text-4xl
+              tracking-tight
+              font-extrabold
+              text-gray-900
+              sm:text-5xl
+              md:text-6xl
+            "
+          >
+            <span class="block xl:inline">😯 ㄨㄚˊ</span><br />
+            <span class="block xl:inline">你想找的</span>
+            <span class="block text-violet-600 xl:inline">文字片段</span>
+            <span class="block xl:inline">不存在</span>
+          </h1>
+          <p
+            class="
+              mt-3
+              text-base text-gray-500
+              sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto
+              md:mt-5 md:text-xl
+              lg:mx-0
+            "
+          >
+            您所請求的資源不見了，或者是從來沒有存在過。
+          </p>
+          <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div class="rounded-md shadow">
+              <button
+                v-if="$router.history.length"
+                class="
+                  w-full
+                  flex
+                  items-center
+                  justify-center
+                  px-8
+                  py-3
+                  border border-transparent
+                  text-base
+                  font-medium
+                  rounded-md
+                  text-white
+                  bg-violet-600
+                  hover:bg-violet-700
+                  md:py-4 md:text-lg md:px-10
+                "
+                @click="$router.back"
+              >
+                返回上一頁
+              </button>
+            </div>
+            <div class="mt-3 sm:mt-0 sm:ml-3">
+              <router-link
+                class="
+                  w-full
+                  flex
+                  items-center
+                  justify-center
+                  px-8
+                  py-3
+                  border border-transparent
+                  text-base
+                  font-medium
+                  rounded-md
+                  text-violet-700
+                  bg-violet-100
+                  hover:bg-violet-200
+                  md:py-4 md:text-lg md:px-10
+                "
+                to="/"
+              >
+                回首頁
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import dayjs from "dayjs";
+
 // import Prism Editor
 import { PrismEditor } from "vue-prism-editor";
 import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
@@ -69,31 +164,61 @@ export default {
     content() {
       return this.gum.content || "";
     },
-    codeMode() {
+    isCodeMode() {
       return this.gum.doc_type !== "plaintext";
-    }
+    },
+    gumTimes() {
+      return {
+        created_at: dayjs(this.gum.created_at * 1000).format(
+          "YYYY-MM-DD HH:mm:ss"
+        ),
+        updated_at: dayjs(this.gum.updated_at * 1000).format(
+          "YYYY-MM-DD HH:mm:ss"
+        ),
+      };
+    },
   },
   data: () => ({
     gum: {},
+    isNotFound: false,
   }),
   methods: {
     highlighter(code) {
       return highlight(code, languages.js); // languages.<insert language> to return html with markup
     },
+    getTimeTitle(key) {
+      switch (key) {
+        case "created_at":
+          return "建立時間";
+        case "updated_at":
+          return "更新時間";
+        default:
+          return key;
+      }
+    },
   },
   created() {
     // Fetch data
-    this.$axios.get(`/anonymous/${this.code}`).then((response) => {
-      this.gum = response.data;
-    });
+    this.$axios
+      .get(`/anonymous/${this.code}`)
+      .then((response) => {
+        this.gum = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.isNotFound = true;
+      });
   },
   mounted() {
-    // Disable edit mode
-    const editorTextarea = this.$refs.codeEditor?.$el?.children[1]?.children[0];
-    if (editorTextarea) {
-      editorTextarea.setAttribute("disabled", true);
-    } else {
-      console.error("editorTextarea is not found.");
+    if (this.isCodeMode) {
+      // Disable edit mode
+      const editorTextarea =
+        this.$refs.codeEditor?.$el?.children[1]?.children[0];
+      if (editorTextarea) {
+        editorTextarea.setAttribute("disabled", true);
+      } else {
+        console.error("editorTextarea is not found.");
+      }
     }
   },
 };
